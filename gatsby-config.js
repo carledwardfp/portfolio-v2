58403984d05657ad703f342d9e79b22dd571bff5
official-carledwardfp <official.carlpahuyo@gmail.com>
2021-02-20 21:59:07 +0800

    fist commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,11 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `CE - Portfolio`,
+    description: `A webpage to showcase my skills.`,
+    author: `@official-carledwardfp`,
+  },
   plugins: [
     `gatsby-image`,
     `gatsby-plugin-fontawesome-css`,
